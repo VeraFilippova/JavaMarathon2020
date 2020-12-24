162dd4b313c3ba5d642a19723205fc5027f62398
Vera <vera.yu.filippova@gmail.com>
2020-12-24 09:34:30 +0300

    complete day1 tasks

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,11 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        int i = 1;
+        int k = 4;
+        while (i <= 9) {
+            System.out.println(i + " * " + k + " = " + (i * k));
+            i++;
+        }
     }
 }
