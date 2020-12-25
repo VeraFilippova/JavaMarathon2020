9d626c4e648efcfb98ddf24c6a4a4d0d7c12874c
Vera <vera.yu.filippova@gmail.com>
2020-12-25 20:34:11 +0300

    complete day2 tasks

--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,26 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int i =a;
+
+        if (a > b) {
+            System.out.println("Некорректный ввод");
+        }
+        while (i<b){
+            if(i>a){
+                if (((i % 5 == 0) && !((i % 10) == 0) && (i > a))) {
+                    System.out.print(i + " ");
+                }
+            }
+            i++;
+        }
 
     }
 }
